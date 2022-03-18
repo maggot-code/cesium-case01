37e72c3e1e21cb2bfadd85cfc5aef5f6af9e663d
maggot_code <wowhuanhuan233@163.com>
2022-03-18 18:11:43 +0800

    fix :-1:

--- src/pages/cesium/cesium.vue
+++ src/pages/cesium/cesium.vue
@@ -2,10 +2,18 @@
  * @Author: maggot-code
  * @Date: 2022-03-07 17:02:12
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-03-18 14:08:15
+ * @LastEditTime: 2022-03-18 18:01:44
  * @Description: file content
 -->
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+import { onMounted } from 'vue';
+import { useMars3d } from 'biz/mars3d/hooks/use-mars3d';
+
+onMounted(async () => {
+    const { map } = await useMars3d("cesium-vessel");
+    console.log(map);
+});
+</script>
 
 <template>
     <!-- <cesium-vessel>
@@ -15,7 +23,7 @@
             <cesium-control></cesium-control>
         </template>
     </cesium-vessel>-->
-    <div class="cesium-vessel" id="cesium-vessel">13</div>
+    <div class="cesium-vessel" id="cesium-vessel"></div>
 </template>
 
 <style scoped lang='scss'>
