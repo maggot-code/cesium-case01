e232f74850c97e403b9f1383b25b9554abbb329c
maggot_code <wowhuanhuan233@163.com>
2022-03-18 14:09:50 +0800

    fix :-1:

--- src/pages/cesium/cesium.vue
+++ src/pages/cesium/cesium.vue
@@ -2,24 +2,10 @@
  * @Author: maggot-code
  * @Date: 2022-03-07 17:02:12
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-03-17 13:23:13
+ * @LastEditTime: 2022-03-18 14:08:15
  * @Description: file content
 -->
-<script setup lang='ts'>
-import { onMounted, onUnmounted } from 'vue';
-import { useMars3d } from 'biz/mars3d/hooks/use-mars3d';
-
-const { marsMap } = useMars3d();
-
-console.log(marsMap);
-
-onMounted(() => {
-    marsMap.onMount("cesium-vessel");
-});
-onUnmounted(() => {
-    marsMap.onDestroy();
-});
-</script>
+<script setup lang='ts'></script>
 
 <template>
     <!-- <cesium-vessel>
@@ -29,7 +15,7 @@ onUnmounted(() => {
             <cesium-control></cesium-control>
         </template>
     </cesium-vessel>-->
-    <div class="cesium-vessel" id="cesium-vessel"></div>
+    <div class="cesium-vessel" id="cesium-vessel">13</div>
 </template>
 
 <style scoped lang='scss'>
